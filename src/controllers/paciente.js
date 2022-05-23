2859ed13666151eeb903843ac0ec023fdcab6f30
Rafael Sangali <rafaelsangali@outlook.com>
2022-05-23 10:59:25 -0300

    feat: CRUD pacientes realizado e testado

--- src/controllers/paciente.js
+++ src/controllers/paciente.js
@@ -1,38 +1,94 @@
+const { Paciente } = require("../models");
+
 const PacienteController = {
-    index: (req, res) => {
-        res.json([]);
+    index: async(req, res) => {
+        const todosPaciente = await Paciente.findAll();
+        res.json(todosPaciente);
     },
 
 
-    store: (req, res) => {
-        res.json(req.body);
+    store: async(req, res) => {
+        const { nome, email, idade, } = req.body;
+        const emailExistente = await Paciente.count({ where: { email, } });
+
+        if (emailExistente == 0) {
+            const novoPaciente = await Paciente.create({
+                nome,
+                email,
+                idade
+            })
+            res.json(novoPaciente)
+        } else {
+            return res.status(400).json({
+                "Mensagem do erro": "E-mail existente",
+            })
+        }
     },
 
 
-    show: (req, res) => {
-        const { id } = req.params;
+    show: async(req, res) => {
+        const { id, } = req.params;
+        const paciente = await Paciente.findByPk(id)
+        const { nome, email, idade } = paciente;
+
+        if (!paciente) {
+            return res.status(404).json({
+                "mensagem do erro": "Id não encontrado",
+            });
+        }
+
 
         res.json({
             id,
-            nome: `Paciente ${id}`,
-            email: "fabricio.paciente@email.com",
-            idade: "06/12/1997"
-        });
+            nome,
+            email,
+            idade
+        })
     },
 
-
-    update: (req, res) => {
+    update: async(req, res) => {
         const { id } = req.params;
+        const { nome, email, idade } = req.body;
 
-        res.json({
-            id,
-            ...(req.body || {}),
+        const paciente = await Paciente.findByPk(id);
+
+        if (!paciente) {
+            return res.status(404).json({
+                "Mensagem do erro": "Id não encontrado",
+            });
+        }
+
+        await Paciente.update({
+            nome,
+            email,
+            idade
+        }, {
+            where: { id, }
         });
+
+        const pacienteAtualizado = await Paciente.findByPk(id)
+        res.json(pacienteAtualizado);
     },
 
 
-    destroy: (req, res) => {
-        res.status(204).send("");
+    destroy: async(req, res) => {
+        const { id } = req.params;
+        const paciente = await Paciente.findByPk(id);
+
+        if (!paciente) {
+            return res.status(404).json({
+                "Mensagem do erro": "Id não encontrado",
+            });
+        }
+
+        try {
+            await paciente.destroy();
+            res.status(204).send();
+
+        } catch (error) {
+            console.log(error.message);
+            res.status(500).json({ "Mensagem de erro": "Este paciente não pôde ser deletado" });
+        }
     },
 };
 
