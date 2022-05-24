73de81a450ea34f65c873cc88a85d3b895a5a424
Brenno-Savio <brennosavio@hotmail.com>
2022-05-24 17:29:05 -0300

    adicionando validators para o paciente e o atendimento

---
+++ src/validators/paciente/store-update.js
@@ -0,0 +1,9 @@
+const { validate, Joi } = require("express-validation");
+
+module.exports = validate({
+    body: Joi.object({
+        nome: Joi.string().min(3).required(),
+        email: Joi.string().email().required(),
+        idade: Joi.string().isoDate().required(),
+    }),
+});
