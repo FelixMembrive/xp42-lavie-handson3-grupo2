06dca248dcefc5f0801fa1a090bf01add163b22e
Brenno-Savio <brennosavio@hotmail.com>
2022-05-25 21:13:09 -0300

    retirando os comentarios

--- src/controllers/atendimento.js
+++ src/controllers/atendimento.js
@@ -41,16 +41,3 @@ const AtendimentoController = {
 };
 
 module.exports = AtendimentoController;
-
-//Função para formatar a saída de uma data ISO
-// const formatarData = (dataISO) => {
-//     const doisDigitos = (umDigito) => {
-//         return umDigito < 10 ? '0' + umDigito : umDigito;
-//     }
-//     dataISO = new Date(dataISO)
-//     const diaDoMes = dataISO.getDate();
-//     const mes = dataISO.getMonth(); // Muito cuidado, Janeiro é o mesmo que 0, não 1.
-//     const ano = dataISO.getFullYear();
-//     const dataFormatada = doisDigitos(diaDoMes) + "/" + doisDigitos(mes + 1) + "/" + ano;
-//     return dataFormatada
-// }
