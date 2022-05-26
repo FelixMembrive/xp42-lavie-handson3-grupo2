09ab99048d1f349687071a7546b7bfb829245052
Brenno-Savio <brennosavio@hotmail.com>
2022-05-25 21:12:13 -0300

    consertando a exibição dos atendimentos

--- src/controllers/atendimento.js
+++ src/controllers/atendimento.js
@@ -3,18 +3,13 @@ const { Atendimento, Paciente, Psicologo } = require("../models");
 const AtendimentoController = {
     
     index: async(req, res) => {
-        const todosAtendimentos = await Atendimento.findAll({include: Paciente});
+        const todosAtendimentos = await Atendimento.findAll({attributes: ["id", "psicologo_id", "data_atendimento", "obs" ], include: Paciente});
         res.status(200).json(todosAtendimentos);
-
-
     },
 
     store: async(req, res) => {
         const { paciente_id, data_atendimento, obs } = req.body;
-        
         const pacienteExistente = await Paciente.count({ where: { id: paciente_id }})
-
-        console.log(pacienteExistente)
 
         if (pacienteExistente != 0 ){
             const novoAtendimento = await Atendimento.create({
@@ -46,3 +41,16 @@ const AtendimentoController = {
 };
 
 module.exports = AtendimentoController;
+
+//Função para formatar a saída de uma data ISO
+// const formatarData = (dataISO) => {
+//     const doisDigitos = (umDigito) => {
+//         return umDigito < 10 ? '0' + umDigito : umDigito;
+//     }
+//     dataISO = new Date(dataISO)
+//     const diaDoMes = dataISO.getDate();
+//     const mes = dataISO.getMonth(); // Muito cuidado, Janeiro é o mesmo que 0, não 1.
+//     const ano = dataISO.getFullYear();
+//     const dataFormatada = doisDigitos(diaDoMes) + "/" + doisDigitos(mes + 1) + "/" + ano;
+//     return dataFormatada
+// }
